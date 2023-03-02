f5394cbd24267d4709414c6ae5e890dc39fda3c2
GhoriRushal <ghorirushal6@gmail.com>
2023-03-02 11:57:18 +0530

    start project

---
+++ assets/index.js
@@ -0,0 +1,26 @@
+function onImg(){
+    this.setAttribute("src","../image/shirt1.png")
+}
+function outImg(){
+    this.setAttribute("src","../image/shirt.png")
+}
+function onImg1(){
+    this.setAttribute("src","../image/women shirt1.png")
+}
+function outImg1(){
+    this.setAttribute("src","../image/women shirt.png")
+}
+function onImg2(){
+    this.setAttribute("src","../image/hoodie1.png")
+}
+function outImg2(){
+    this.setAttribute("src","../image/hoodie.png")
+}
+function onImg3(){
+    this.setAttribute("src","../image/track pants1.png")
+}
+function outImg3(){
+    this.setAttribute("src","../image/track pants.png")
+}
+
+// hover navbar//
